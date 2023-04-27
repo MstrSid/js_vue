f5b8bca3d1e605725430c9c75d5d6ec5f065731a
Yauhen Kasmachus <mstrsid@live.com>
2023-04-27 12:41:47 +0300

    task001 done

---
+++ task001.js
@@ -0,0 +1,10 @@
+'use strict';
+console.log(+(Math.PI).toFixed(2));
+
+console.log(Math.max(15, 11, 16, 12, 51, 12, 13, 51));
+console.log(Math.min(15, 11, 16, 12, 51, 12, 13, 51));
+
+console.log(Math.floor(Math.random() * (9)));
+console.log(+(0.6 + 0.7).toFixed(1));
+console.log(Number.parseInt('100$'));
+
